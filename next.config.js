d7067893fb8f17a200441a1c79b943a3499fdf70
Thimira Nirmal <timnirmal@gmail.com>
2022-11-23 07:46:09 +0530

    hotfiix/netxjsconfig

--- next.config.js
+++ next.config.js
@@ -1,12 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  experimental:{ appDir: true },
-  // reactStrictMode: false,
-  typescript: {
-    ignoreBuildErrors: true
-  }
+    reactStrictMode: true,
+    swcMinify: true,
+    experimental: {
+        appDir: true, images: {
+            unoptimized: true
+        }
+    },
+    // reactStrictMode: false,
+    typescript: {
+        ignoreBuildErrors: true
+    }
 }
 
 module.exports = nextConfig
